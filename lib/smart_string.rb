1b95b5e13d504c5afc84d62fb9480a90bc201919
Viktar Makayed <vmakoed@gmail.com>
2021-09-23 19:06:26 +0200

    Extract validations

--- lib/smart_string.rb
+++ lib/smart_string.rb
@@ -1,14 +1,35 @@
 # frozen_string_literal: true
 
 require_relative './smart_character'
-require_relative './logging'
+require_relative './validations'
 
 class SmartString
+  include Validations
+
   attr_reader :source
 
   def initialize(source)
     @source = source
   end
+
+  def check_balance
+    opening = find_opening
+    closing = find_closing
+
+    return if no_specials?(opening, closing)
+    return if only_opening?(opening, closing)
+    return if only_closing?(opening, closing)
+    return if closing_before_opening?(opening, closing)
+
+    reverse_closing = find_reverse_closing
+
+    return if opening_does_not_match_reverse_closing?(opening, reverse_closing)
+
+    empty!(opening, reverse_closing)
+    check_balance
+  end
+
+  private
 
   def find_opening
     characters.find(&:opening?)
@@ -19,47 +40,16 @@ class SmartString
   end
 
   def find_reverse_closing
-    characters.reverse.find(&:closing?)
+    reverse_characters.find(&:closing?)
   end
 
   def empty!(*unwanted_characters)
     unwanted_characters.each(&:empty!)
   end
 
-  def check_balance
-    opening = find_opening
-    closing = find_closing
-
-    return log_balanced if [opening, closing].all?(&:nil?)
-
-    unless opening.nil?
-      return log_imbalanced('unclosed', opening) if closing.nil?
-    end
-
-    unless closing.nil?
-      return log_imbalanced('unopened', closing) if opening.nil?
-    end
-
-    return log_imbalanced('unopened', closing) if closing.before?(opening)
-
-    reverse_closing = find_reverse_closing
-    return log_imbalanced('unclosed', opening) unless opening.matches?(reverse_closing)
-
-    empty!(opening, reverse_closing)
-    check_balance
+  def reverse_characters
+    @reverse_characters ||= characters.reverse
   end
-
-  def log_balanced
-    Logging.log_balanced(source: source)
-  end
-
-  def log_imbalanced(issue, smart_character)
-    Logging.log_imbalanced(
-      source: source, issue: issue, smart_character: smart_character
-    )
-  end
-
-  private
 
   def characters
     @characters ||= source.split('').map.with_index do |character, index|
